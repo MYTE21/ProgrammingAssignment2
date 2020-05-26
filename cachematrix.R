f9b25691c5c84c5a027ce6071144f3bcad4ef94b
Md. Yasmi Tohabar Evon (2016831021) <silverevon10010@gmail.com>
2020-05-27 00:50:38 +0600

    Refactor naming and comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,36 +1,41 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## This function creates a special "matrix", x 
-## that can cache its inverse.
+## This function creates a special "matrix" - x, 
+## that can cache its inverse. set ans/inverse "inverse"
+## as NULL. It can get-set the matrix , inverse matrix 
+## also create list of those
 
 makeCacheMatrix <- function(x = matrix()) {
-  s <- NULL
+  inverse <- NULL
   set <- function(y) {
     x <<- y
-    s <<- NULL
+    inverse <<- NULL
   }
   get <- function() x
-  setsolve <- function(solve) s <<- solve
-  getsolve <- function() s
+  setInverse <- function(solveMatrix) inverse <<- solveMatrix
+  getInverse <- function() inverse
   list(set = set, get = get,
-       setsolve = setsolve,
-       getsolve = getsolve)
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
 ## This function computes the inverse of the special "matrix" 
-## returned by makeCacheMatrix .
+## returned by makeCacheMatrix . 
+## Validate if the matrix "x" is inversed or not 
+## If "yes" then return the matrix or
+## inverse it and then return
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
-  s <- x$getsolve()
-  if(!is.null(s)) {
-    message("getting inversed matrix")
-    return(s)
+  inverse <- x$getInverse()
+  if(!is.null(inverse)) {
+    message("Getting Inversed Matrix ... !")
+    return(inverse)
   }
   data <- x$get()
-  s <- solve(data, ...)
-  x$setsolve(s)
-  s
+  inverse <- solve(data, ...)
+  x$setInverse(inverse)
+  inverse
 }
